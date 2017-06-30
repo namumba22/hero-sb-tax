74622454c9512b31f489ca5c8b9c0a8eef01acaf
Andriy Dumchykov <Andriy_Dumchykov@epam.com>
2017-06-30 13:38:19 +0300

    init

--- src/main/java/com/myproj/RounderClient.java
+++ src/main/java/com/myproj/RounderClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("${tax.rounder-service-name-service-name}")
+@FeignClient("${tax.rounder-service-name}")
 public interface RounderClient {
 
     //TODO: replace with SPEL
